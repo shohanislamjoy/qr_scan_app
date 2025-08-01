dd0f36a8c6452034540c9850453c963ebe88647c
Shohan Islam Joy <shohanislamjoy@gmail.com>
2025-08-02 03:12:13 +0600

    Create lib/main.dart with initial Flutter app code

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'scan_qr_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'QR Scan App',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ScanQrPage(),
+    );
+  }
+}
